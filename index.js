f1a85aa4b720aea0e4f5ce46e5aded8957839814
Robson Batista <rbatista@ecentry.com>
2018-07-06 12:50:15 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-var items = document.querySelectorAll('[data-async-rclass="search"] .rlfl__tls > div[jstcache]');
 
-window.list = window.list || [];
+var instance = null;
 
-function normalize(text){
-    return text.replace(/\n/g, '').trim()
+function scraping(){
+    instance = document.querySelector('[data-async-rclass="search"] .rlfl__tls')
+    var items = document.querySelectorAll('[data-async-rclass="search"] .rlfl__tls > div[jstcache]');
+
+    window.list = window.list || [];
+
+    function normalize(text){
+        return text.replace(/\n/g, '').trim()
+    }
+
+    items.forEach(function(item){
+        var heading = normalize(item.querySelector("[role=heading]").innerText);
+        var location = normalize(item.querySelectorAll(".rllt__details > div")[2].innerText);
+        var phone = normalize(item.querySelectorAll(".rllt__details > div")[3].innerText);
+        window.list.push(heading + "; " + location + "; " + phone)
+    });
+
+    var time = setInterval(function(){
+        if(document.querySelector('[data-async-rclass="search"] .rlfl__tls') !== instance){
+            scraping();
+        }
+    }, 1000);
+
+
+    document.querySelector("[role=presentation] td.cur").nextElementSibling.querySelector('a').click();
 }
 
-items.forEach(function(item){
-    var heading = normalize(item.querySelector("[role=heading]").innerText);
-    var location = normalize(item.querySelectorAll(".rllt__details > div")[2].innerText);
-    var phone = normalize(item.querySelectorAll(".rllt__details > div")[3].innerText);
 
-    window.list.push(heading + ", " + location + ", " + phone)
-});
+scraping();
